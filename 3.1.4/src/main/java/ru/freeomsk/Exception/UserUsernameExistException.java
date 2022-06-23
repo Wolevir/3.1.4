059ab5f93eb0944f5fe62f23e73dac9e82d9e208
Wolevir <100397898+Wolevir@users.noreply.github.com>
2022-06-23 14:40:52 +0400

    Add files via upload

---
+++ 3.1.4/src/main/java/ru/freeomsk/Exception/UserUsernameExistException.java
@@ -0,0 +1,9 @@
+package ru.freeomsk.Exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+
+public class UserUsernameExistException extends DataIntegrityViolationException {
+    public UserUsernameExistException(String msg) {
+        super(msg);
+    }
+}
